d0930d5034aa2af18ced7330ffbb17f082ac7b7c
EdijsNiks <edijsniks@gmail.com>
2024-09-16 19:25:03 +0300

    Login and Reg screens UI done

--- screens/LoginReg/Registration.js
+++ screens/LoginReg/Registration.js
@@ -1,34 +1,37 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    Image,
-    KeyboardAvoidingView,
-    TextInput,
-    TouchableOpacity,
-    Pressable,
-  } from "react-native";
-  import React, { useEffect, useState } from "react";
-  import { useNavigation } from "@react-navigation/native";
-  
-  //import axios from "axios";
-  const Registration = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigation = useNavigation();
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const [user, setUser] = useState([]);
-  
-    const togglePasswordVisibility = () => {
-      setPasswordVisible(!passwordVisible);
-    };
-    const userData = {
-      email: email,
-      password: password,
-    };
-  
-    
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Dimensions,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const Registration = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
+  const navigation = useNavigation();
+  const { width, height } = Dimensions.get("window");
+
+  const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
+
+  const userData = {
+    email: email,
+    password: password,
+  };
+
   /*
     const handleLogin = (email, password) => {
       axios
@@ -52,107 +55,210 @@ import {
           console.log(error);
         });
     };
-  */ 
-  
-    return (
-      <SafeAreaView
-        style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
-      >
-        <View>
-          <Image
-            style={{ top: 100 }}
-            source={require("../../assets/favicon.png")}
+*/
+  const validateEmail = (email) => {
+    // Ensure email has an "@" symbol and is at least 5 characters long
+    const emailPattern = /.+@.+\..+/;
+    return email.length >= 5 && emailPattern.test(email);
+  };
+
+  const validatePassword = (password) => {
+    // Ensure password is at least 5 characters long and contains at least one digit
+    const passwordPattern = /^(?=.*\d).{5,}$/;
+    return passwordPattern.test(password);
+  };
+
+  const validateFields = () => {
+    if (!name || !email || !password || !confirmPassword) {
+      Alert.alert("Error", "All fields are required.");
+      return false;
+    }
+    if (!validateEmail(email)) {
+      Alert.alert("Error", "Invalid email format. Please enter a valid email.");
+      return false;
+    }
+    if (!validatePassword(password)) {
+      Alert.alert(
+        "Error",
+        "Password must be at least 5 characters long and contain at least one number."
+      );
+      return false;
+    }
+    if (password !== confirmPassword) {
+      Alert.alert("Error", "Passwords do not match.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleRegister = () => {
+    if (validateFields()) {
+      Alert.alert("Success", "Registration completed!", [], {
+        cancelable: true,
+      });
+
+      // Show success popup for 2 seconds, then navigate to Login
+      setTimeout(() => {
+        navigation.replace("Login");
+      }, 2000);
+    }
+  };
+
+  return (
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <KeyboardAvoidingView style={styles.container}>
+        {/* Image */}
+        <Image
+          source={require("../../assets/logo1.png")}
+          style={[styles.image, { width: width - 20, height: height * 0.2 }]}
+          resizeMode="contain"
+        />
+
+        {/* Name Input */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={name}
+            onChangeText={(text) => setName(text)}
+            style={styles.input}
+            placeholder="Name"
           />
         </View>
-        <KeyboardAvoidingView>
-          <View>
-            <Text
-              style={{
-                fontSize: 30,
-                marginTop: 200,
-                width: 350,
-                fontWeight: "bold",
-              }}
-            >
-              Welcome!
-            </Text>
-          </View>
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              backgroundColor: "white",
-              paddingVertical: 5,
-              borderRadius: 8,
-              border: "solid black",
-              borderWidth: 2,
-              gap: 5,
-            }}
-          >
-            <TextInput
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-              style={{ fontSize: 20, width: 350 }}
-              placeholder="Email Address"
-            />
-          </View>
-          <View
-            style={{
-              marginTop: 30,
-              flexDirection: "row",
-              backgroundColor: "white",
-              paddingVertical: 5,
-              borderRadius: 8,
-              gap: 5,
-              border: "solid black",
-              borderWidth: 2,
-            }}
-          >
-            <TextInput
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              secureTextEntry={!passwordVisible}
-              style={{ fontSize: 20, width: 350 }}
-              placeholder="Password"
-            />
-            <TouchableOpacity>
-              <Image
-                style={{ position: "absolute", right: 5, height: 32 }}
-                source={require("../../assets/favicon.png")}
-                onPress={togglePasswordVisibility}
-              />
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              paddingVertical: 5,
-            }}
-          >
-            <Text style={{ fontSize: 20, width: 350, color: "#FE8F4E" }}>
-              {" "}
-              Forgot password?
-            </Text>
-          </View>
-        </KeyboardAvoidingView>
-        <View>
-          <Pressable onPress={() => handleLogin(email, password)}>
-            <Image
-              style={{
-                top: 30,
-                width: 300,
-                borderRadius: 20,
-                shadowColor: "black",
-              }}
-              source={require("../../assets/favicon.png")}
-            />
-          </Pressable>
+
+        {/* Email Input */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={styles.input}
+            placeholder="Email Address"
+            keyboardType="email-address"
+          />
         </View>
-      </SafeAreaView>
-    );
-  };
-  
-  export default Registration;
-  
-  const styles = StyleSheet.create({});
+
+        {/* Password Input */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={!passwordVisible}
+            style={styles.input}
+            placeholder="Password"
+          />
+          <TouchableOpacity onPress={togglePasswordVisibility}>
+            <Text style={styles.toggleText}>
+              {passwordVisible ? "Hide" : "Show"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Confirm Password Input */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={confirmPassword}
+            onChangeText={(text) => setConfirmPassword(text)}
+            secureTextEntry={!passwordVisible}
+            style={styles.input}
+            placeholder="Confirm Password"
+          />
+          <TouchableOpacity onPress={togglePasswordVisibility}>
+            <Text style={styles.toggleText}>
+              {passwordVisible ? "Hide" : "Show"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Register Button */}
+        <Pressable
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
+          onPress={handleRegister}
+        >
+          <LinearGradient
+            colors={isPressed ? ["#A4D337", "#A4D337"] : ["#A4D337", "#7CB518"]}
+            style={styles.loginButton}
+          >
+            <Text style={styles.loginButtonText}>Register</Text>
+          </LinearGradient>
+        </Pressable>
+
+        {/* Registration Prompt */}
+        <View style={styles.registrationContainer}>
+          <Text style={styles.registrationText}>
+            Have an account?{" "}
+            <Text
+              style={styles.registrationLink}
+              onPress={() => navigation.navigate("Login")}
+            >
+              Press Here
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    marginBottom: 20,
+  },
+  inputContainer: {
+    marginTop: 10,
+    flexDirection: "row",
+    backgroundColor: "white",
+    paddingVertical: 5,
+    borderRadius: 8,
+    borderColor: "black",
+    borderWidth: 2,
+    width: 350,
+  },
+  input: {
+    fontSize: 20,
+    width: 300,
+    paddingHorizontal: 10,
+  },
+  toggleText: {
+    fontSize: 18,
+    color: "#A4D337",
+    marginRight: 20,
+  },
+  loginButton: {
+    marginTop: 20,
+    width: 350,
+    paddingVertical: 15,
+    borderRadius: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  loginButtonText: {
+    fontSize: 20,
+    color: "black",
+    fontWeight: "bold",
+  },
+  registrationContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  registrationText: {
+    fontSize: 16,
+    color: "black",
+    fontWeight: "bold",
+  },
+  registrationLink: {
+    color: "#A4D337",
+    fontWeight: "bold",
+  },
+});
+
+export default Registration;
